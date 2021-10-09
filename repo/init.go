80b04043dadab1e4b4393f3e284b483562981a8f
Amit <amit.vickey5@gmail.com>
2021-10-09 22:42:50 +0530

    prod mongo

--- repo/init.go
+++ repo/init.go
@@ -12,7 +12,7 @@ var repoClient *mongo.Client
 
 func InitRepoClient() error {
 	var err error
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb+srv://amit:9mnYIInwK7yDu0IF@cluster0.l0avf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	repoClient, err = mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		return errors.Wrap(err, "error-connecting-to-mongodb-repo")
